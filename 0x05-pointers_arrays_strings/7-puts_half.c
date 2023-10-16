cd27e5085a235a2e03a8f45c0ea6c2ef9690c72d
Hadija-S-Mahmoud <hsmahmoud20@gmail.com>
2023-10-16 12:54:58 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ for (a = 0; str[a] != '\0'; a++)
 c++;
 b = (c / 2);
 if ((c % 2) == 1)
-b = (( c+ 1) / 2);
+b = ((c + 1) / 2);
 for (a = b; str[a] != '\0'; a++)
 _putchar(str[a]);
 _putchar('\n');
