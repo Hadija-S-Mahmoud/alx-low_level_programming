99ce8148a7ffa76a70c1fc0b6d0d2d3b8898b4a4
Hadija-S-Mahmoud <hsmahmoud20@gmail.com>
2023-11-21 11:49:06 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 /**
 * get_nodeint_at_index - returns the nth node of a listint_t linked list.
 * @head: pointer
-* index: index
+* @index: index
 * Return: pointer or NULL
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
-* @head; pointer
+* @head: pointer
 * Return: 0 if the list is empty
 */
 int sum_listint(listint_t *head)
